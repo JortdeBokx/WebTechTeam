db8c59e138c23586ab2d2c8e5e1a442e5bfb3b78
sander102907 <s.d.bruin@hotmail.com>
2018-01-05 17:14:03 +0100

    Working on search bar and cards on home

--- Markis/static/js/base.js
+++ Markis/static/js/base.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+$(document).ready(() => {
+
+  $('#search-subject').on('keyup', function(){
+    var input, filter, title, a, i;
+    input = document.getElementById('search-subject');
+    filter = input.value.toUpperCase();
+    title = getElementsByTagName('h4');
+    console.log("title");
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < title.length; i++) {
+        a = title[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            title[i].style.display = "";
+        } else {
+            title[i].style.display = "none";
+        }
+    }
+  });
+
+})
