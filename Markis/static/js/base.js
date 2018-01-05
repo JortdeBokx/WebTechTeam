8b6cf0735f26b610a82e0a0317cfd60891b26554
sander102907 <s.d.bruin@hotmail.com>
2018-01-05 18:22:16 +0100

    Search bar working and card-decks changed to card-columns (still some issues)

--- Markis/static/js/base.js
+++ Markis/static/js/base.js
@@ -1,19 +1,19 @@
 $(document).ready(() => {
 
   $('#search-subject').on('keyup', function(){
-    var input, filter, title, a, i;
+    var input, filter, title, code, name, i;
     input = document.getElementById('search-subject');
     filter = input.value.toUpperCase();
-    title = getElementsByTagName('h4');
-    console.log("title");
+    card = document.getElementsByClassName('card');
 
     // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < title.length; i++) {
-        a = title[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            title[i].style.display = "";
+    for (i = 0; i < card.length; i++) {
+        code = card[i].getElementsByTagName("h4")[0];
+        name = card[i].getElementsByTagName("p")[0];
+        if (code.innerHTML.toUpperCase().indexOf(filter) > -1 || name.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            card[i].style.display = "";
         } else {
-            title[i].style.display = "none";
+            card[i].style.display = "none";
         }
     }
   });
