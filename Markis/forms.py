b70445734a8d5e5dcf400414976f1db17548a794
sander102907 <s.d.bruin@hotmail.com>
2018-01-06 14:39:35 +0100

    register en login pagina's gestyled

--- Markis/forms.py
+++ Markis/forms.py
@@ -1,16 +1,16 @@
 from wtforms import Form, StringField, PasswordField, BooleanField, validators
 
 class registerForm(Form):
-	firstname = StringField("First Name", [validators.Length(min=1, max=20)])
-	lastname = StringField("Last Name", [validators.Length(min=1, max=20)])
-	email = StringField("E-mail", [validators.Email(message="Please enter a valid email address")])
-	username = StringField("Username", [validators.Length(min=3, max=35)])
-	password = PasswordField("Password", [validators.Length(min=8, max=64)])
+	firstname = StringField("First Name", [validators.Length(min=1, max=20)], render_kw={"placeholder": "First Name"})
+	lastname = StringField("Last Name", [validators.Length(min=1, max=20)], render_kw={"placeholder": "Last Name"})
+	email = StringField("E-mail", [validators.Email(message="Please enter a valid email address")], render_kw={"placeholder": "Email"})
+	username = StringField("Username", [validators.Length(min=3, max=35)], render_kw={"placeholder": "Username"})
+	password = PasswordField("Password", [validators.Length(min=8, max=64)], render_kw={"placeholder": "Password"})
 	password2 = PasswordField("Confirm Password", [
 		validators.Length(min=8, max=64),
 		validators.EqualTo('password', message="Both passwords need to match!")
-	])
+	], render_kw={"placeholder": "Password Confirmation"})
 class loginForm(Form):
-	username = StringField("Username", [validators.Length(min=3, max=35)])
-	password = PasswordField("Password", [validators.Length(min=8, max=64)])
+	username = StringField("Username", [validators.Length(min=3, max=35)], render_kw={"placeholder": "Username"})
+	password = PasswordField("Password", [validators.Length(min=8, max=64)], render_kw={"placeholder": "Password"})
 	keepLoggedIn = BooleanField("")
