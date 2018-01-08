77e9bb8302b7b147772151210bdb7ad828d3f010
bram49 <schut49@gmail.com>
2018-01-08 15:57:55 +0100

    Made pdf viewer larger

--- Markis/static/js/base.js
+++ Markis/static/js/base.js
@@ -61,6 +61,7 @@ $(document).ready(() => {
             card[i].style.display = "none";
         }
     }
+
   });
 
   //Search clear button
@@ -72,17 +73,14 @@ $(document).ready(() => {
     }).on('touchstart click', '.onX', function( ev ){
         ev.preventDefault();
         $(this).removeClass('x onX').val('').change();
-				if (document.getElementById("search-subject")) {
-					searchSubject();
-				} else {
-					searchFile();
-				}
+        searchSubject();
+        searchFile();
   });
 
 /*
 * This is the plugin
 */
-(function(a){a.createModal=function(b){defaults={title:"",message:"Modal created",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 780px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
+(function(a){a.createModal=function(b){defaults={title:"",message:"Modal created",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 780px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog modal-lg">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
 
 /*
 * Here is how you use it
