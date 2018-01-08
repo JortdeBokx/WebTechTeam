25ee9abd1a23cec84547cf4233660fbfa747ea98
Jort de Bokx <jort.debokx@gmail.com>
2018-01-08 15:17:28 +0100

    Added name and title functionality for subject page

--- Markis/app.py
+++ Markis/app.py
@@ -6,7 +6,7 @@ import os
 from urllib.parse import urlparse, urljoin
 
 from flask import Flask, render_template, request, send_from_directory, redirect, url_for, flash, abort, Response
-from flask_login import LoginManager, login_required, login_user, logout_user
+from flask_login import LoginManager, login_user, logout_user
 from passlib.hash import sha256_crypt
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
@@ -44,38 +44,39 @@ login_manager.login_message = "You need to be logged in to view this page!"
 
 @app.route('/')
 def home():
-    # Get subjects from Database
-    conn = engine.connect()
+	# Get subjects from Database
+	conn = engine.connect()
 
-    rv = conn.execute("""SELECT subject_id, subject_name FROM subjects ORDER BY subject_id ASC""").fetchall()
-    conn.close()
-    return render_template('home.html', subjects=rv)
+	rv = conn.execute("""SELECT subject_id, subject_name FROM subjects ORDER BY subject_id ASC""").fetchall()
+	conn.close()
+	return render_template('home.html', subjects=rv)
 
 @app.route('/uploadfile', methods=["GET", "POST"])
 def uploadFile():
-    if request.method == "POST":
-        if 'file' not in request.files:
-            flash('No selected items')
-            return "Err"
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected')
-            return "Err"
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                                filename=filename))
+	if request.method == "POST":
+		if 'file' not in request.files:
+			flash('No selected items')
+			return "Err"
+		file = request.files['file']
+		if file.filename == '':
+			flash('No file selected')
+			return "Err"
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('uploaded_file',
+												filename=filename))
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-        return send_from_directory(app.config['UPLOAD_FOLDER'],
-                                filename)
+		return send_from_directory(app.config['UPLOAD_FOLDER'],
+								filename)
 
-@app.route('/subject',)
+@app.route('/subject/<subjectid>',)
 #@login_required
-def subject():
-	return render_template('subject.html')
+def subject(subjectid):
+	#TODO: if subject does not exist, return 404
+	return render_template('subject.html', subjectDataSet = getSubjectData(subjectid))
 
 @app.route('/profile',)
 #@login_required
@@ -85,60 +86,60 @@ def profile():
 @app.route('/favorites')
 #@login_required
 def favorites():
-    return render_template('favorites.html')
+	return render_template('favorites.html')
 
 @app.route("/logout")
 def logout():
-    logout_user()
-    return Response('<p>Logged out</p>')
+	logout_user()
+	return Response('<p>Logged out</p>')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = registerForm(request.form)
-    if request.method == 'POST' and form.validate():
-        conn = engine.connect()
+	form = registerForm(request.form)
+	if request.method == 'POST' and form.validate():
+		conn = engine.connect()
 
 
-        username = form.username.data
-        first_name = form.firstname.data
-        last_name = form.lastname.data
-        email = form.email.data
-        password = sha256_crypt.hash(form.password.data)
-        s = text("INSERT INTO users (first_name, last_name, username, password, email) VALUES (:f, :l, :u, :p, :e)")
-        rv = conn.execute(s, f=first_name, l = last_name, u=username, p=password, e=email)
-        conn.close()
-        if str(rv):
-            return redirect(url_for('login'))
-    else:
-        return render_template('register.html', form=form)
+		username = form.username.data
+		first_name = form.firstname.data
+		last_name = form.lastname.data
+		email = form.email.data
+		password = sha256_crypt.hash(form.password.data)
+		s = text("INSERT INTO users (first_name, last_name, username, password, email) VALUES (:f, :l, :u, :p, :e)")
+		rv = conn.execute(s, f=first_name, l = last_name, u=username, p=password, e=email)
+		conn.close()
+		if str(rv):
+			return redirect(url_for('login'))
+	else:
+		return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = loginForm(request.form)
-    if request.method == 'POST' and form.validate():
+	form = loginForm(request.form)
+	if request.method == 'POST' and form.validate():
 
-        # Get the password Hash from  the DB where username
-        conn = engine.connect()
-        s = text("SELECT id, password FROM users WHERE username=:u")
-        rv = conn.execute(s, u= str(form.username.data) ).fetchall()
-        conn.close()
-        #return str(rv[0]['password'])
-        if sha256_crypt.verify(form.password.data, str(rv[0]['password'])):
-            user = User(rv[0]['id'])
-            user.authenticate(form.username.data)
-            login_user(user)
-        else:
-            return "Wrong password"
+		# Get the password Hash from  the DB where username
+		conn = engine.connect()
+		s = text("SELECT id, password FROM users WHERE username=:u")
+		rv = conn.execute(s, u= str(form.username.data) ).fetchall()
+		conn.close()
+		#return str(rv[0]['password'])
+		if sha256_crypt.verify(form.password.data, str(rv[0]['password'])):
+			user = User(rv[0]['id'])
+			user.authenticate(form.username.data)
+			login_user(user)
+		else:
+			return "Wrong password"
 
-        flash('Logged in successfully.')
+		flash('Logged in successfully.')
 
-        next = request.args.get('next')
-        if not is_safe_url(next):
-            return abort(400)
+		next = request.args.get('next')
+		if not is_safe_url(next):
+			return abort(400)
 
-        return redirect(next or url_for('home'))
-    else:
-        return render_template('login.html', form=form)
+		return redirect(next or url_for('home'))
+	else:
+		return render_template('login.html', form=form)
 
 
 #############################################
@@ -147,12 +148,12 @@ def login():
 
 @app.route('/css/<path:filename>')
 def css(filename):
-    return send_from_directory('css',
-                               filename)
+	return send_from_directory('css',
+							   filename)
 @app.route('/js/<path:filename>')
 def javascript(filename):
-    return send_from_directory('js',
-                               filename)
+	return send_from_directory('js',
+							   filename)
 
 #############################################
 #			   Helper Functions 			#
@@ -160,18 +161,25 @@ def javascript(filename):
 
 @login_manager.user_loader
 def load_user(userid):
-    return User(userid)
+	return User(userid)
 
 def is_safe_url(target):
-    ref_url = urlparse(request.host_url)
-    test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and \
-           ref_url.netloc == test_url.netloc
+	ref_url = urlparse(request.host_url)
+	test_url = urlparse(urljoin(request.host_url, target))
+	return test_url.scheme in ('http', 'https') and \
+		   ref_url.netloc == test_url.netloc
 
 def allowed_file(filename):
-        return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+		return '.' in filename and \
+		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def getSubjectData(subjectid):
+	facultyid = int(subjectid[0])
+	conn = engine.connect()
+	s = text("SELECT subject_id, subject_name, faculty_name FROM subjects, faculties WHERE subject_id=:u and faculty_id = :i")
+	rv = conn.execute(s, u=subjectid, i=facultyid).fetchone()
+	conn.close()
+	return rv
 
 #############################################
 #               Data Objects 	        	#
@@ -179,41 +187,41 @@ def allowed_file(filename):
 
 class User:
 
-    def __init__(self, uid):
-        self.is_authenticated = False
-        self.is_active = False
-        self.is_anonymous = True
-        self.username = None
-        self.user_id = uid
+	def __init__(self, uid):
+		self.is_authenticated = False
+		self.is_active = False
+		self.is_anonymous = True
+		self.username = None
+		self.user_id = uid
 
-    def __repr__(self):
-        return "%d/%s" % (self.user_id, self.username)
+	def __repr__(self):
+		return "%d/%s" % (self.user_id, self.username)
 
-    def get_id(self):
-        conn = engine.connect()
-        s = text("SELECT id FROM users WHERE username=:u")
-        rv = conn.execute(s, u=self.username).fetchall()
-        conn.close()
-        return str(rv[0]['id'])
+	def get_id(self):
+		conn = engine.connect()
+		s = text("SELECT id FROM users WHERE username=:u")
+		rv = conn.execute(s, u=self.username).fetchall()
+		conn.close()
+		return str(rv[0]['id'])
 
-    def authenticate(self, username):
-        self.is_authenticated = True
-        self.is_active = True
-        self.is_anonymous = False
-        self.username = username
+	def authenticate(self, username):
+		self.is_authenticated = True
+		self.is_active = True
+		self.is_anonymous = False
+		self.username = username
 
-    def setUsername(self, username):
-        self.username = username
+	def setUsername(self, username):
+		self.username = username
 
-    def get(user_id):
-        return self
+	def get(user_id):
+		return self
 
-    def returnUsername(self):
-        return self.username
+	def returnUsername(self):
+		return self.username
 
 #############################################
 #   Fasten your seatbelts, here we go! 		#
 #############################################
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
