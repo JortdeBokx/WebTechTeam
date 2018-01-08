d85aa9de6a25e4ce3f186ffdbaa8ad1b36d297c7
Jort de Bokx <jort.debokx@gmail.com>
2018-01-08 16:15:32 +0100

    Added error handling

--- Markis/app.py
+++ Markis/app.py
@@ -164,6 +164,24 @@ def uploaded_file(filename):
                                 filename)
 
 #############################################
+#			     Error Pages    			#
+#############################################
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(418)
+def page_not_found(e):
+    return render_template('418.html'), 418
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+#############################################
 #			   Helper Functions 			#
 #############################################
 
