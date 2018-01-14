02c5909bc9e72564ad1a64ec7d23a83033a4ae78
Jort de Bokx <jort.debokx@gmail.com>
2018-01-14 17:41:19 +0100

    Removed redundant removal of records that are cascaded by the database

--- Markis/app.py
+++ Markis/app.py
@@ -242,10 +242,6 @@ def removeFile():
 				if FileExistsByID(fileid):
 					removeFileFromDisk(fileid)
 					conn = engine.connect()
-					s = text("DELETE FROM user_file_favorite WHERE file_ID = :f")
-					rv = conn.execute(s, f=fileid)
-					s = text("DELETE FROM user_file_vote WHERE file_ID = :f")
-					rv = conn.execute(s, f=fileid)
 					s = text("DELETE FROM files WHERE file_ID = :f")
 					rv = conn.execute(s, f=fileid)
 					conn.close()
